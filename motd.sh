0fa3f48cb006edd290d8a45127734da453816427
Antoine Boucher <antoine.bou13@gmail.com>
2023-03-09 23:17:50 -0500

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+# AlgoETS ASCII art
+echo "                                                             "
+echo "                                                             "
+echo "  █████╗ ██╗      ██████╗  ██████╗ ███████╗████████╗▄▄███▄▄· "
+echo " ██╔══██╗██║     ██╔════╝ ██╔═══██╗██╔════╝╚══██╔══╝██╔════╝ "
+echo " ███████║██║     ██║  ███╗██║   ██║█████╗     ██║   ███████╗ "
+echo " ██╔══██║██║     ██║   ██║██║   ██║██╔══╝     ██║   ╚════██║ "
+echo " ██║  ██║███████╗╚██████╔╝╚██████╔╝███████╗   ██║   ███████║ " 
+echo " ╚═╝  ╚═╝╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝   ╚═▀▀▀══╝ "
+echo "                                                             "                                     
+echo "                                                             "
+
+# Update motd
+echo "Welcome to AlgoETS Server!"
+echo "To access the AlgoETS platform, please visit https://algoets.ca"
+echo ""
+
+# Save motd to /etc/motd
+echo "Welcome to AlgoETS Server!" > /etc/motd
+echo "To access the AlgoETS platform, please visit https://algoets.ca" >> /etc/motd
+echo "" >> /etc/motd
